f12bc6c1e8eaa9cf100a0d64213b2f2712f6466c
Jinghui Liao <jinghui@wayne.edu>
2023-05-08 09:40:21 -0400

    sgx

--- benchmark/javascript/bench.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-node bench.js $1 $2
--- wasm/nginx_conf/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -it --rm -p 8000:80 \
-  -v $(pwd)/../../docs:/usr/share/nginx/html \
-  -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf \
-  nginx
